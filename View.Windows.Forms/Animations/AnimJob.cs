aab02608cb9585742554def0f9e24d11a853f80a
View <yangwei169@live.com>
2020-08-07 16:11:57 +0800

    添加 View.Windows.Forms 项目

---
+++ View.Windows.Forms/Animations/AnimJob.cs
@@ -0,0 +1,18 @@
+﻿namespace View.Windows.Forms.Animations
+{
+    /// <summary>
+    /// 动画对象
+    /// </summary>
+    class AnimJob
+    {
+        /// <summary>
+        /// 动画的属性
+        /// </summary>
+        public Properties Property;
+
+        /// <summary>
+        /// 动画的值
+        /// </summary>
+        public object Value;
+    }
+}
---
+++ View.Windows.Forms/Animations/Properties.cs
@@ -0,0 +1,38 @@
+﻿namespace View.Windows.Forms.Animations
+{
+    /// <summary>
+    /// 控件对应的属性
+    /// </summary>
+    public enum Properties
+    {
+        /// <summary>
+        /// 控件的左边缘与其容器工作区的左边缘之间的距离
+        /// </summary>
+        Left,
+        /// <summary>
+        /// 控件的上边缘与其容器工作区的上边缘之间的距离
+        /// </summary>
+        Top,
+        /// <summary>
+        /// 控件的宽度
+        /// </summary>
+        Width,
+        /// <summary>
+        /// 控件的高度
+        /// </summary>
+        Height,
+        /// <summary>
+        /// 控件的背景色
+        /// </summary>
+        Backcolor,
+        /// <summary>
+        /// 控件的前景色
+        /// </summary>
+        Forecolor,
+        /// <summary>
+        /// 窗体的不透明级别
+        /// <para>此属性仅用于窗体</para>
+        /// </summary>
+        Opacity
+    }
+}
