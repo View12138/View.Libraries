3c96674d92a0946b065cc94e06e31e968daf6ad8
View <yangwei169@live.com>
2020-08-13 13:40:01 +0800

    新增 View.Core 项目

---
+++ View.Core/Extensions/EnumExtension.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace View.Core.Extensions
+{
+    /// <summary>
+    /// 枚举扩展
+    /// </summary>
+    public static class EnumExtension
+    {
+        /// <summary>
+        /// 移除位域枚举中指定的值
+        /// </summary>
+        /// <typeparam name="TEnum"></typeparam>
+        /// <param name="_enum"></param>
+        /// <param name="enum"></param>
+        /// <returns></returns>
+        public static TEnum Remove<TEnum>(this TEnum _enum, TEnum @enum) where TEnum : struct
+        {
+            if (!typeof(TEnum).IsEnum) { throw new ArgumentException("TEnum must be an enumerated type."); }
+
+            int _value = Convert.ToInt32(_enum);
+            int value = Convert.ToInt32(@enum);
+
+            return (TEnum)Enum.ToObject(typeof(TEnum), _value & (~value));
+
+        }
+    }
+}
---
+++ View.Core/Extensions/StringExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace View.Core.Extensions
+{
+    /// <summary>
+    /// 字符串扩展
+    /// </summary>
+    public static class StringExtension
+    {
+        /// <summary>
+        /// 指示指定的字符串是 <see langword="null"/> 还是 <see cref="string.Empty"/> 字符串。
+        /// </summary>
+        /// <param name="value">指定的字符串</param>
+        /// <returns>如果参数 <paramref name="value"/> 为 <see langword="null"/> 或 空字符串("")，则为 <see langword="true"/>；否则为 <see langword="false"/>。</returns>
+        public static bool IsNullOrEmpty(this string value) => string.IsNullOrEmpty(value);
+        /// <summary>
+        /// 指示指定的字符串是 <see langword="null"/> 、空还是仅由空白字符串组成。
+        /// </summary>
+        /// <param name="value">指定的字符串</param>
+        /// <returns>如果参数 <paramref name="value"/> 为 <see langword="null"/> ，空字符串("")或仅由空白字符组成，则为 <see langword="true"/>；否则为 <see langword="false"/>。</returns>
+        public static bool IsNullOrWhiteSpace(this string value) => string.IsNullOrWhiteSpace(value);
+    }
+}
