b89c3959080bc76e8891e2b5901f78ea0335df2c
View <yangwei169@live.com>
2020-09-24 10:36:59 +0800

    test

--- View.Core/Extensions/EnumExtension.cs
+++ View.Core/Extensions/EnumExtension.cs
@@ -13,17 +13,17 @@ namespace View.Core.Extensions
         /// 移除位域枚举中指定的值
         /// </summary>
         /// <typeparam name="TEnum"></typeparam>
-        /// <param name="_enum"></param>
         /// <param name="enum"></param>
+        /// <param name="_enum"></param>
         /// <returns></returns>
-        public static TEnum Remove<TEnum>(this TEnum _enum, TEnum @enum) where TEnum : struct
+        public static TEnum Remove<TEnum>(this Enum @enum, TEnum _enum) where TEnum : Enum
         {
             if (!typeof(TEnum).IsEnum) { throw new ArgumentException("TEnum must be an enumerated type."); }
 
-            int _value = Convert.ToInt32(_enum);
             int value = Convert.ToInt32(@enum);
-
-            return (TEnum)Enum.ToObject(typeof(TEnum), _value & (~value));
+            int _value = Convert.ToInt32(_enum);
+            
+            return (TEnum)Enum.ToObject(typeof(TEnum), value & (~_value));
 
         }
 
--- View.Core/Values/DateTimeValue.cs
+++ View.Core/Values/DateTimeValue.cs
@@ -12,12 +12,12 @@ namespace View.Core.Values
         /// <summary>
         /// new <see cref="DateTime"/> 的值。
         /// </summary>
-        public static DateTime Null = new DateTime();
+        public static DateTime Null => new DateTime();
 
         /// <summary>
         /// Unix 时间戳 0 的值。
         /// <para>ISO 8601规范 : 1970-01-01T00:00:00Z</para>
         /// </summary>
-        public static DateTime UnixZero = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        public static DateTime UnixZero => new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
     }
 }
