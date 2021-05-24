969882597edb1cafdca93f573106581fba69b2dd
View <yangwei169@live.com>
2021-05-24 16:27:40 +0800

    test: update

--- tests/View.Drawing.Extensions.Test/ImageExtensionTest.cs
+++ tests/View.Drawing.Extensions.Test/ImageExtensionTest.cs
@@ -20,7 +20,7 @@ namespace View.Drawing.Extensions.Test
             Image image24 = Image.FromFile(path24);
             try
             {
-                if (!System.IO.Directory.Exists(@"Out")) 
+                if (!System.IO.Directory.Exists(@"Out"))
                 { System.IO.Directory.CreateDirectory(@"Out"); }
 
                 Size minSizeA = new Size(64, 40);
@@ -43,30 +43,30 @@ namespace View.Drawing.Extensions.Test
 
         private void BuildImage(Image image24, Size minSizeA, string groupName)
         {
-            var newMinImgToMin = image24.ChangeSize(minSizeA, new ChangeSizeConfiguration() { Reserve = true, SizeMode = Models.SizeMode.ToMin });
+            var newMinImgToMin = image24.ChangeSize(minSizeA, new Configurations.ChangeSizeConfiguration() { SizeMode = Models.SizeMode.ToMin });
             Output.WriteLine(newMinImgToMin.Size.ToString());
             newMinImgToMin.Save($@"Out\View.Logo.24.{groupName}.ToMin.jpg");
             Assert.True(newMinImgToMin.Size.Width >= minSizeA.Width && newMinImgToMin.Height >= minSizeA.Height);
 
-            var newMinImgToMax = image24.ChangeSize(minSizeA, new ChangeSizeConfiguration() { Reserve = true, SizeMode = Models.SizeMode.ToMax });
+            var newMinImgToMax = image24.ChangeSize(minSizeA, new Configurations.ChangeSizeConfiguration() { SizeMode = Models.SizeMode.ToMax });
             Output.WriteLine(newMinImgToMax.Size.ToString());
             newMinImgToMax.Save($@"Out\View.Logo.24.{groupName}.ToMax.jpg");
             Assert.True(newMinImgToMax.Size.Width <= minSizeA.Width && newMinImgToMax.Height <= minSizeA.Height);
 
-            var newMinImgStretch = image24.ChangeSize(minSizeA, new ChangeSizeConfiguration() { Reserve = true, SizeMode = Models.SizeMode.Stretch });
+            var newMinImgStretch = image24.ChangeSize(minSizeA, new Configurations.ChangeSizeConfiguration() { SizeMode = Models.SizeMode.Stretch });
             Output.WriteLine(newMinImgStretch.Size.ToString());
             newMinImgStretch.Save($@"Out\View.Logo.24.{groupName}.Stretch.jpg");
             Assert.True(newMinImgStretch.Size.Width == minSizeA.Width && newMinImgStretch.Height == minSizeA.Height);
 
-            var newMinImgCut1 = image24.ChangeSize(minSizeA, new ChangeSizeConfiguration() { Reserve = true, SizeMode = Models.SizeMode.Cut, Location = new Point(0, 0) });
+            var newMinImgCut1 = image24.ChangeSize(minSizeA, new Configurations.ChangeSizeConfiguration() { SizeMode = Models.SizeMode.Cut, Location = new Point(0, 0) });
             Output.WriteLine(newMinImgCut1.Size.ToString());
             newMinImgCut1.Save($@"Out\View.Logo.24.{groupName}.Cut1.jpg");
             Assert.True(newMinImgCut1.Size.Width == minSizeA.Width && newMinImgCut1.Height == minSizeA.Height);
-            var newMinImgCut2 = image24.ChangeSize(minSizeA, new ChangeSizeConfiguration() { Reserve = true, SizeMode = Models.SizeMode.Cut, Location = new Point(15, 25), ClipInBox = true });
+            var newMinImgCut2 = image24.ChangeSize(minSizeA, new Configurations.ChangeSizeConfiguration() { SizeMode = Models.SizeMode.Cut, Location = new Point(15, 25), ClipInBox = true });
             Output.WriteLine(newMinImgCut2.Size.ToString());
             newMinImgCut2.Save($@"Out\View.Logo.24.{groupName}.Cut2.jpg");
             Assert.True(newMinImgCut2.Size.Width == minSizeA.Width && newMinImgCut2.Height == minSizeA.Height);
-            var newMinImgCut3 = image24.ChangeSize(minSizeA, new ChangeSizeConfiguration() { Reserve = true, SizeMode = Models.SizeMode.Cut, Location = new Point(15, 25), ClipInBox = false });
+            var newMinImgCut3 = image24.ChangeSize(minSizeA, new Configurations.ChangeSizeConfiguration() { SizeMode = Models.SizeMode.Cut, Location = new Point(15, 25), ClipInBox = false });
             Output.WriteLine(newMinImgCut3.Size.ToString());
             newMinImgCut3.Save($@"Out\View.Logo.24.{groupName}.Cut3.jpg");
             Assert.True(newMinImgCut3.Size.Width == minSizeA.Width && newMinImgCut3.Height == minSizeA.Height);
@@ -84,7 +84,7 @@ namespace View.Drawing.Extensions.Test
                 {
                     if (!ImageHandle.IsErrorPixelFormat(pixel))
                     {
-                        using (var temp = image24.ChangePixelFormat(pixel, new ChangeSizeConfiguration() { Reserve = true }))
+                        using (var temp = image24.ChangePixelFormat(pixel))
                         {
                             try
                             {
@@ -96,7 +96,7 @@ namespace View.Drawing.Extensions.Test
                                 Output.WriteLine($"Change Error Pixel: {pixel}");
                             }
                         }
-                        using (var temp = image32.ChangePixelFormat(pixel, new ChangeSizeConfiguration() { Reserve = true }))
+                        using (var temp = image32.ChangePixelFormat(pixel))
                         {
                             try
                             {
